6404c3ca286295015fe905a19dd9db93b1792f3a
musawi <96937618+Musawi154@users.noreply.github.com>
2022-04-21 00:02:15 +0700

    Add files via upload

--- Transformation_GUI.py
+++ Transformation_GUI.py
@@ -19,7 +19,7 @@ def fileLoad():
     global v
     fileName = filedialog.askopenfilename(initialdir="/",
                                           title="Select a File",
-                                          filetype=(("csv files", "*.csv"),("All Files", "*.*")))
+                                          filetype=(("csv files", "*.csv"), ("All Files", "*.*")))
     print(fileName)
     v.set(fileName)
     labelFile["text"] = fileName
@@ -33,21 +33,24 @@ def loadData():
         dataLoad = pd.read_csv(csv_data)
         dataLoad.columns = ['No', 'Easting', 'Northing', 'Elevation', 'Remarks']
         return dataLoad
+    except FileNotFoundError:
+        tk.messagebox.showerror("Error", "Please choose the data")
     except:
         tk.messagebox.showerror("Error", "Please choose the correct data")
 
-#----------- Menu dialog ----------
+
+# ----------- Menu dialog ----------
 menuFrame = tk.LabelFrame(root, text="MENU")
 menuFrame.place(height=110, width=450, rely=0.0, relx=0.03)
 # ---------------- Menu Button
 tk.Label(menuFrame, text='File Path').grid(row=0, column=0, padx=10, pady=5)
 v = tk.StringVar()
 entry = tk.Entry(menuFrame, textvariable=v).grid(row=0, column=1, columnspan=4)
-openButton = tk.Button(menuFrame, text='BROWSE DATA',command=lambda : fileLoad())
+openButton = tk.Button(menuFrame, text='BROWSE DATA', command=lambda: fileLoad())
 openButton.grid(row=1, column=0, padx=10, pady=5)
-procesButton = tk.Button(menuFrame, text='PROCESS',command=lambda : procesStart())
+procesButton = tk.Button(menuFrame, text='PROCESS', command=lambda: procesStart())
 procesButton.grid(row=1, column=2, padx=10, pady=5)
-saveButton = tk.Button(menuFrame, text='SAVE DATA',command=lambda : SaveFile())
+saveButton = tk.Button(menuFrame, text='SAVE DATA', command=lambda: SaveFile())
 saveButton.grid(row=1, column=3, padx=10, pady=5)
 
 
@@ -63,7 +66,7 @@ choiceTwo = ttk.Radiobutton(choiceFrame, text='Transformasi dari UTM ke lokal si
 choiceTwo.place(rely=0.45, relx=0.1)
 
 # ----------- TREEVIEW FRAME -----------
-#Frame Treeview 1 (data input)
+# Frame Treeview 1 (data input)
 inputFrame = tk.LabelFrame(root, text="DATA INPUT")
 inputFrame.place(height=300, width=500, rely=0.60, relx=0)
 # Treeview data input
@@ -75,7 +78,7 @@ inputTreeview.configure(xscrollcommand=treescrollxInput.set, yscrollcommand=tree
 treescrollxInput.pack(side="bottom", fill="x")
 treescrollyInput.pack(side="right", fill="y")
 
-#Frame Treeview 2 (Hasil Transformasi)
+# Frame Treeview 2 (Hasil Transformasi)
 transFrame = tk.LabelFrame(root, text="HASIL TRANSFORMASI")
 transFrame.place(height=300, width=500, rely=0.60, relx=0.5)
 # Treeview transformasi
@@ -87,7 +90,7 @@ transTreeview.configure(xscrollcommand=treescrollxTrans.set, yscrollcommand=tree
 treescrollxTrans.pack(side="bottom", fill="x")
 treescrollyTrans.pack(side="right", fill="y")
 
-#Frame Treeview 3 (Deviasi Jarak)
+# Frame Treeview 3 (Deviasi Jarak)
 devFrame = tk.LabelFrame(root, text="DEVIASI PERBANDINGAN DISTORSI")
 devFrame.place(height=130, width=500, rely=0.43, relx=0.5)
 # Treeview transformasi
@@ -101,15 +104,16 @@ treescrollyDev.pack(side="right", fill="y")
 
 
 # ----------- FRAME FIGURE UNTUK HASIL PLOTTING -----------
-#Frame Figure
+# Frame Figure
 figureFrame = tk.LabelFrame(root, text="PLOTTING")
 figureFrame.place(height=340, width=500, rely=0.0, relx=0.5)
 
 
 # ----------- FRAME RESUME PENGOLAHAN DATA -----------
-#Frame resume
+# Frame resume
 resumeFrame = tk.LabelFrame(root, text="RESUME PENGOLAHAN DATA")
 resumeFrame.place(height=300, width=450, rely=0.22, relx=0.03)
+
 
 class labelResume:
     def __init__(self, dx, text, row):
@@ -118,8 +122,8 @@ class labelResume:
         self.row = row
 
     def label(self):
-        lText = tk.Label(resumeFrame, text=self.text).grid(column=0, row=self.row)
-        lText2 = tk.Label(resumeFrame, textvariable=self.dx).grid(column=2, row=self.row)
+        tk.Label(resumeFrame, text=self.text).grid(column=0, row=self.row)
+        tk.Label(resumeFrame, textvariable=self.dx).grid(column=2, row=self.row)
 
 
 dC = tk.IntVar()
@@ -134,12 +138,12 @@ dStd = tk.IntVar()
 devCount = tk.IntVar()
 fCount = tk.IntVar()
 
-labelData = {'dx':[dC, dfC, NArE, NArN, dCount, dMax, dMin, dMean, dStd, devCount, fCount],
-             'text':[" Total data "," Total data setelah di bersihkan "," Total data NA di kolom Easting ",
-                     " Total data NA di kolom Northing "," Total data deviasi"," Deviasi maksimum ",
-                     " Deviasi minimum "," Rata-rata deviasi "," Simpangan Baku deviasi ",
-                     " Data diluar batas Toleransi "," Data dengan pengurutan salah "],
-             'row':[0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11]}
+labelData = {'dx': [dC, dfC, NArE, NArN, dCount, dMax, dMin, dMean, dStd, devCount, fCount],
+             'text': [" Total data ", " Total data setelah di bersihkan ", " Total data NA di kolom Easting ",
+                     " Total data NA di kolom Northing ", " Total data deviasi", " Deviasi maksimum ",
+                     " Deviasi minimum ", " Rata-rata deviasi ", " Simpangan Baku deviasi ",
+                     " Data diluar batas Toleransi ", " Data dengan pengurutan salah "],
+             'row': [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11]}
 
 lDf = pd.DataFrame(labelData)
 
@@ -166,6 +170,7 @@ def processing():
     print(choiceVar.get())
     choice = choiceVar.get()
 
+    parTrans = []
     if choice == "l":
         parTrans = par.L2U()
     elif choice == "u":
@@ -237,7 +242,7 @@ def processing():
 
 
 def procesStart():
-    global dC, NArE, NArN, dfC, dInit, dCount, dMax, dMin, dMean, dStd, devCount, fCount
+    global dC, NArE, NArN, dfC, dInit, dCount, dMax, dMin, dMean, dStd, devCount, fCount, dist_not_ok
     data_init, data, dataMerge = processing()
     NArE.set(na_rowE)
     NArN.set(na_rowN)
@@ -253,21 +258,26 @@ def procesStart():
 
     # Quality control data
     data_comp['Check_no'] = np.where(data_comp['No'] == data_comp['No_T'], True, False)
-    data_comp['Check_Easting'] = np.where(data_comp['Easting'] == data_comp['Easting_T'], 0, data_comp['Easting'] - data_comp['Easting_T'])
-    data_comp['Check_Northing'] = np.where(data_comp['Northing'] == data_comp['Northing_T'], 0, data_comp['Northing'] - data_comp['Northing_T'])
-    data_comp['Check_Elevation'] = np.where(data_comp['Elevation'] == data_comp['Elevation_T'], 0, data_comp['Elevation'] - data_comp['Elevation_T'])
+    data_comp['Check_Easting'] = np.where(data_comp['Easting'] == data_comp['Easting_T'],
+                                          0, data_comp['Easting'] - data_comp['Easting_T'])
+    data_comp['Check_Northing'] = np.where(data_comp['Northing'] == data_comp['Northing_T'],
+                                           0, data_comp['Northing'] - data_comp['Northing_T'])
+    data_comp['Check_Elevation'] = np.where(data_comp['Elevation'] == data_comp['Elevation_T'],
+                                            0, data_comp['Elevation'] - data_comp['Elevation_T'])
     data_comp['Check_Remarks'] = np.where(data_comp['Remarks'] == data_comp['Remarks_T'], True, False)
     data_comp['Distance'] = (((data_comp['Easting'] - 806842.970)**2) + ((data_comp['Northing'] - 9755607.709)**2))**0.5
     data_comp['dev_t'] = (((data_comp['Check_Easting'])**2) + ((data_comp['Check_Northing'])**2))**0.5
     data_comp['S_dist'] = np.multiply(data_comp['Distance'],
-                                              [1 * 0.0007290161672 if dv > 803500.0 else 1 * 0.000585044212 if dv < 800600.0 else 1 * .00070122909528 for dv in data_comp['Easting_T']])
+                                              [1 * 0.0007290161672 if dv > 803500.0
+                                               else 1 * 0.000585044212 if dv < 800600.0
+                                              else 1 * .00070122909528 for dv in data_comp['Easting_T']])
 
     # Calculated descriptive statistic
     data_comp['dev_t'] = data_comp['dev_t'].astype(float).round(decimals=3)
     data_comp['S_dist'] = data_comp['S_dist'].astype(float).round(decimals=3)
     data_comp['dev_d'] = data_comp['dev_t'] - data_comp['S_dist']
     data_comp['dev_d'] = data_comp['dev_d'].astype(float).round(decimals=3)
-    data_show = data_comp[['No','dev_t', 'S_dist', 'dev_d', 'Check_Remarks', 'Remarks_T']]
+    data_show = data_comp[['No', 'dev_t', 'S_dist', 'dev_d', 'Check_Remarks', 'Remarks_T']]
     falseCount = data_comp[data_comp['Check_no'] == False].count()['Check_Remarks']
     fCount.set(falseCount)
     desc_count = round(data_comp['dev_d'].count(), 3)
@@ -286,10 +296,10 @@ def procesStart():
     dist_not_ok.columns = ['No', 'Selisih Koordinat', 'Jarak x faktor skala', 'Deviasi', 'Cek Pengurutan', 'Remarks']
 
     # Plotting to compared distortion between coordinate and distance
-    figure = plt.Figure(figsize=(5,5), dpi=100)
+    figure = plt.Figure(figsize=(5, 5), dpi=100)
     ax = figure.add_subplot(111)
     ax.scatter(data_comp['Distance'], data_comp['dev_t'], marker="s", color='red', label='Distortion from coordinate')
-    ax.scatter(data_comp['Distance'], data_comp['S_dist'], marker="o",color='blue', label='Distortion from distance')
+    ax.scatter(data_comp['Distance'], data_comp['S_dist'], marker="o", color='blue', label='Distortion from distance')
     scatter = FigureCanvasTkAgg(figure, figureFrame)
     scatter.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
     ax.set_xlabel('Distance', fontsize=10)
@@ -298,7 +308,7 @@ def procesStart():
     ax.legend()
 
     clearData()
-    #Treeview Input
+    # Treeview Input
     inputTreeview["column"] = list(data_init.columns)
     inputTreeview["show"] = "headings"
     for column in inputTreeview["column"]:
@@ -309,7 +319,7 @@ def procesStart():
     for row in data_init_row:
         inputTreeview.insert("", "end", values=row)
 
-    #Treeview Transformasi
+    # Treeview Transformasi
     transTreeview["column"] = list(dataMerge.columns)
     transTreeview["show"] = "headings"
     for columnT in transTreeview["column"]:
@@ -320,7 +330,7 @@ def procesStart():
     for rowT in dataMerge_row:
         transTreeview.insert("", "end", values=rowT)
 
-    #Treeview Transformasi
+    # Treeview Transformasi
     devTreeview["column"] = list(dist_not_ok.columns)
     devTreeview["show"] = "headings"
     for columnD in devTreeview["column"]:
@@ -340,8 +350,12 @@ def clearData():
 # ----------- PENYIMPANAN DATA -----------
 def SaveFile():
     data_init, data, dataMerge = processing()
-    extension = [("csv file(*.csv)","*.csv"), ('All tyes(*.*)', '*.*')]
-    SAVING_PATH = filedialog.asksaveasfile(mode='w',filetypes=extension, defaultextension=extension)
+    extension = [("csv file(*.csv)", "*.csv"), ('All types(*.*)', '*.*')]
+    SAVING_PATH = filedialog.asksaveasfile(mode='w', filetypes=extension, defaultextension=extension)
     dataMerge.to_csv(SAVING_PATH, index=False, line_terminator="\n")
+
+    SAVING_PATH_log = filedialog.asksaveasfile(mode='w', filetypes=extension, defaultextension=extension)
+    dist_not_ok.to_csv(SAVING_PATH_log, index=False, line_terminator="\n")
+
 
 root.mainloop()
